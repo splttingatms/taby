7900d4e91d0f4f73365e954570fcb3cac27999af
Sunny Rodriguez <sunny.rodriguez@outlook.com>
2018-11-10 14:19:13 -0800

    Use feature detection
    This commit changes the way Android is supported by doing feature
    detection instead of checking the operating system. This is a more
    flexible implementation when working across browsers and device types.

--- background-script.js
+++ background-script.js
@@ -1,19 +1,13 @@
 console.debug("background-script loaded");
 
-var os = "";
-browser.runtime.getPlatformInfo()
-    .then((info) => {
-        os = info.os;
-    });
-
+// click on browser toolbar button
 browser.browserAction.onClicked.addListener(handleClick);
 
 function handleClick(currentTab) {
-    console.debug("handleClick executed");
-
-    console.debug(`os: ${os}`);
-    if (os.match("android")) {
-        // sidebarAction.open() is not implemented on Firefox for Android
+    if (typeof browser.sidebarAction === 'undefined'
+        || typeof browser.sidebarAction.open == 'undefined') {
+        console.debug("sidebarAction not supported");
+        // sidebarAction is not implemented on Firefox for Android so use Extension Page instead
         // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/sidebarAction/open
         browser.tabs.create({
             url: "/ext-page/page.html"
